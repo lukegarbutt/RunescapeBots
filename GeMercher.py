39c4f74c7eb6b61b4569e7b1dabb7e2b79e2e2d0
LukeGarbutt <luke_garbutt@live.co.uk>
2017-06-25 13:12:31 +0100

    added 3 empty functions to gemercher with comments outlining a rough plan to start out the project

--- GeMercher.py
+++ GeMercher.py
@@ -1,1 +1,13 @@
 # Main script that will merch items in the GE
+
+def detect_runescape_windows():
+	pass # this function will detect how many runescape windows are present and where they are
+
+def move_and_resize_runescape_windows():
+	pass # this will move and resize the detected windows.
+	# Initially this will just pass since we don't know how to do this, but further down the road we can add to this and implement it
+
+def detect_ge_slots_and_member_status():
+	pass # this will take in the location of the windows and scan each one to see whether or not it is a members account
+	# which can be done by checking if there are slots that are unavailable to be used (for example have no buy bag in them)
+	# then for each screen we want to detect how many available slots we have and the location of each
