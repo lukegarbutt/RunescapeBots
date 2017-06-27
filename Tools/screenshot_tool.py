f23579742a47a57b7d2003a4e5857f3517741522
LukeGarbutt <luke_garbutt@live.co.uk>
2017-06-27 03:10:17 +0100

    added more screenshots and worked on starting the main code for gemercher, an autologout preventor has been added

--- Tools/screenshot_tool.py
+++ Tools/screenshot_tool.py
@@ -1,4 +1,5 @@
 import pyautogui
+import time
 
 def take_screenshot():
 	file_name = 'screenshots/' + input("What would you like to save the screenshot as? Please enter the entire file name including extension, ie .png\n")
@@ -6,9 +7,11 @@ def take_screenshot():
 	top_left = pyautogui.position()
 	input("Place curser over the bottom right corner of the box you'd like to screenshot and press enter")
 	bottom_right = pyautogui.position()
+	time.sleep(3)
 	width = bottom_right[0] - top_left[0]
 	height = bottom_right[1] - top_left[1]
 	pyautogui.screenshot(file_name, region=(top_left[0], top_left[1], width, height))
+
 
 if __name__ == '__main__':
 	take_screenshot()
