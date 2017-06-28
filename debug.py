cfff40cb4175c57e7fc54ee98707e0d73329c002
lukegarbutt <luke_garbutt@hotmail.com>
2017-06-28 04:04:28 +0100

    testing something

--- debug.py
+++ debug.py
@@ -1,5 +1,5 @@
 import pyautogui
 
 def main():
-	print(pyautogui.locateOnScreen("Tools/screenshots/items/yew_logs.png"))
+	print(pyautogui.locateOnScreen("Tools/screenshots/items/Rune_bar.png"))
 main()
