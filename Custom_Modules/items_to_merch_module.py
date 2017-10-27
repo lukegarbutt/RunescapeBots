7863dee3c89d17a1d853ff1802ea8c34637c510e
LukeGarbutt <luke_garbutt@live.co.uk>
2017-10-27 17:37:51 +0100

    added a template for the missing module, also other changes with the images that could potentially cause issues

---
+++ Custom_Modules/items_to_merch_module.py
@@ -0,0 +1,8 @@
+# This file is purely to hold the names of the items you want to merch 
+
+def p2p_items():
+	p2p_items = ['items you want to merch in here', 'for example', 'Coal', 'or', 'Maple logs']
+	return(p2p_items)
+def f2p_items():
+	f2p_items = ['same as above, just f2p only items']
+	return(f2p_items)
