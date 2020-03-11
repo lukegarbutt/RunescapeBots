2e94fc00a766650f1ca328e6cba61df87de134d0
eric <eric@ericehmke.com>
2020-03-11 19:17:25 -0400

    Beginning OSRS rewrite

--- Custom_Modules/items_to_merch_module.py
+++ Custom_Modules/items_to_merch_module.py
@@ -1,7 +1,7 @@
 # This file is purely to hold the names of the items you want to merch 
 
 def p2p_items():
-	p2p_items = ['items you want to merch in here', 'for example', 'Coal', 'or', 'Maple logs']
+	p2p_items = ['Coal', 'Cannonball', 'Lobster', 'Iron ore']
 	return(p2p_items)
 def f2p_items():
 	f2p_items = ['same as above, just f2p only items']
